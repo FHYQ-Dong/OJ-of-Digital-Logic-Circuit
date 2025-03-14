ce0525d9e2e8b213c3f74501aee8681b87e74896
FHYQ-Dong <FHYQ-Dong-Work@outlook.com>
2025-03-14 14:03:38 +0800

    update some answers and add code to crawl answers in verilog_oj_crawler.py

--- crawler/README.md
+++ crawler/README.md
@@ -11,11 +11,17 @@ pip install -r requirements.txt
 ```
 
 2. Run the script
-
+  - If you only want to crawl all the problems:
 ```bash
 # Run in the parent directory of crawler/
-python crawler/verilog_oj_crawler.py -u <url_to_oj> -d .
+python crawler/verilog_oj_crawler.py --url <url_to_oj> -d .
+```
+  - If you want to crawl all problems and your AC-answers (only one AC-answer per problem will be crawled):
+```bash
+# Run in the parent directory of crawler/
+python crawler/verilog_oj_crawler.py --url <url_to_oj> -d . -a -u <your_oj_username> -p <your_oj_password>
 ```
 
-3. You will find some folders under the parent directory of `crawler/`. Each folder contains one problem (of course, no answers).
-4. If you want to re-crawl the problems, remove `crwaled_problems.json` under the parent directory of `crawler/`.
+3. You will find some folders under the parent directory of `crawler/` (can be changed through argument `--dest`/`-d`). Each folder contains one problem.
+
+4. If you want to re-crawl the problems, remove `crawled.json` under the parent directory of `crawler/` (the directory specified through `--dest`/`-d`).
--- crawler/verilog_oj_crawler.py
+++ crawler/verilog_oj_crawler.py
@@ -1,14 +1,14 @@
 import requests
 import loguru
 from tqdm import tqdm
-import os
+import os, sys
 from pathlib import Path
 import concurrent.futures
 import json
 import mistletoe.markdown_renderer
 
 
-class MDParser(mistletoe.markdown_renderer.MarkdownRenderer, requests.Session):
+class MDVParser(mistletoe.markdown_renderer.MarkdownRenderer, requests.Session):
     def __init__(self, logger):
         mistletoe.markdown_renderer.MarkdownRenderer.__init__(self)
         requests.Session.__init__(self)
@@ -28,7 +28,7 @@ class MDParser(mistletoe.markdown_renderer.MarkdownRenderer, requests.Session):
             if child.children is not None:
                 self.get_img_recursive(token.children[i], dest)
 
-    def to_markdown(self, problem_data: dict, dest: Path):         
+    def to_md(self, problem_data: dict, dest: Path):         
         if not os.path.exists((dest / 'assets').as_posix()):
             os.makedirs((dest / 'assets').as_posix(), exist_ok=True)
         md = '---\n'
@@ -56,30 +56,105 @@ class MDParser(mistletoe.markdown_renderer.MarkdownRenderer, requests.Session):
             f.write(md)
         return md
 
+    def to_v(self, problem_data: dict, dest: Path):
+        if not os.path.exists(dest.as_posix()):
+            os.makedirs(dest.as_posix(), exist_ok=True)
+        v = f'/*\n'
+        v += f' * id: {problem_data["id"]}\n'
+        v += f' * logic_id: {problem_data["logic_id"]}\n'
+        v += f' * name: {problem_data["name"]}\n'
+        v += f' * level: {problem_data["level"]}\n'
+        v += f' * tags: {problem_data["tags"]}\n'
+        v += f' * points: {problem_data["points"]}\n'
+        v += f' * answer_id: {problem_data["answer_id"]}\n'
+        v += f' * answer_file_id: {problem_data["answer_file_id"]}\n'
+        v += f' */\n\n'
+        v += problem_data["answer"]
+        with open((dest / 'answer.v').as_posix(), 'w', encoding='utf-8') as f:
+            f.write(v)
+        return v
+
+
 class VerilogOJCrawler(requests.Session):
     def __init__(
         self, 
         base_url, 
-        log_file=None
+        get_answer=False,
+        **kwargs
     ):
+        """
+        The crawler for Verilog OJ.
+        
+        ### Args:
+        - `base_url`: str, the base url of the OJ, should be like http://xxx.xxx.xxx.xxx/oj
+        - `get_answer`: bool, whether to get the answer of the problems
+        - `**kwargs`: 
+          - `username`: str, the username of the OJ (needed if `get_answer` is True)
+          - `password`: str, the password of the OJ (needed if `get_answer` is True)
+          - `log_file`: str, the log file path (default: None)
+        """
         super(VerilogOJCrawler, self).__init__()
         self.base_url = base_url
         self.logger = loguru.logger
+        log_file = kwargs.get('log_file', None)
+        if log_file is not None:
+            os.makedirs(Path(log_file).parent.as_posix(), exist_ok=True)
+        else:
+            os.makedirs((Path(__file__).parent / 'log').as_posix(), exist_ok=True)
+        self.logger.remove()
         self.logger.add(log_file if log_file is not None else (Path(__file__).parent / 'log/crawler.log').as_posix(), rotation='5 MB')
-        self.md_parser = MDParser(self.logger)
+        self.logger.add(sys.stderr, level='INFO')
+        self.md_parser = MDVParser(self.logger)
         self.logger.info('Crawler started.')
+        self.user_id = None
+        if get_answer:
+            username, password = kwargs.get('username', None), kwargs.get('password', None)
+            if username is None or password is None:
+                self.logger.error('Username and password are needed to get answers.')
+                raise ValueError('Username and password are needed to get answers.')
+            if not self.login(username, password):
+                raise ValueError('Failed to login.')
+            self.logger.info('Login success.')
+        self.logger.info('Crawler initialized.')
+        self.problems, self.answers = {}, {}
+        self.crawled_problems, self.crawled_answers = set(), set()
         
+        
+    def login(self, username: str, password: str):
+        resp = self.post(f"{self.base_url}/api/user/login", data={'username': username, 'password': password})
+        if not resp.ok:
+            self.logger.error(f'Failed to login: {resp.text}')
+            return False
+        resp = self.get(f"{self.base_url}/api/user/status-login")
+        if not resp.ok:
+            self.logger.error(f'Failed to check login status: {resp.text}')
+            return False
+        data = resp.json()
+        if data['isLoggedIn']:
+            self.logger.info(f'Login success: {data["username"]}')
+            self.user_id = data['userID']
+            return True
+        else:
+            self.logger.error(f'Login failed: {data["username"]}')
+            return False
+
     
-    def get_all_problems(self, dest='problems', workers=1):
+    def get_all(self, dest='problems', workers=1):
         dest = Path(dest)
-        crwaled_problems = []
         os.makedirs(dest.as_posix(), exist_ok=True)
-        if not os.path.exists((dest / 'crwaled_problems.json').as_posix()):
-            with open((dest / 'crwaled_problems.json').as_posix(), 'w', encoding='utf-8') as f:
-                f.write('[]')
-        crwaled_problems = set(json.load(open((dest / 'crwaled_problems.json').as_posix(), 'r', encoding='utf-8')))
-            
-        problems = []
+        if not os.path.exists((dest / 'crawled.json').as_posix()):
+            with open((dest / 'crawled.json').as_posix(), 'w', encoding='utf-8') as f:
+                f.write(json.dumps({
+                    'crawled_problems': [],
+                    'crawled_answers': []
+                }))
+        else:
+            with open((dest / 'crawled.json').as_posix(), 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                self.crawled_problems = set(data['crawled_problems'])
+                self.crawled_answers = set(data['crawled_answers'])
+         
+        # get problem list   
         resp = self.get(f'{self.base_url}/api/problems/?limit=15&offset=0')
         if not resp.ok:
             self.logger.error(f'Failed to get problems: {resp.text}')
@@ -94,42 +169,78 @@ class VerilogOJCrawler(requests.Session):
                 return
             data = resp.json()
             for prob in data['results']:
-                problems.append({
+                self.problems[prob['logic_id']] = {
                     'id': prob['id'],
                     'logic_id': prob['logic_id'],
                     'name': prob['name'],
                     'level': prob['level'],
                     'tags': prob['tags'],
                     'points': prob['total_grade'],
-                })
-                
+                }
+        
+        # get problem details        
         with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
-            tasks = [executor.submit(self.get_problem, prob) for prob in problems if prob['logic_id'] not in crwaled_problems]
-            problems = []
+            tasks = [executor.submit(self.get_problem, prob) for logic_id, prob in self.problems.items() if logic_id not in self.crawled_problems]
             pbar = tqdm(total=len(tasks), ncols=100, ascii=True)
+            pbar.set_description('Getting problem logic_id: None')
             for task in concurrent.futures.as_completed(tasks):
                 data = task.result()
                 if data is None:
                     pbar.update(1)
                     continue
-                problems.append(data)
-                pbar.set_description(f'Getting problem id: {data["logic_id"]}')
-                md = self.md_parser.to_markdown(data, dest / f'{data["logic_id"]}-{data["name"]}')
+                self.problems[data['logic_id']].update(data)
+                pbar.set_description(f'Getting problem logic_id: {data["logic_id"]}')
+                md = self.md_parser.to_md(data, dest / f'{data["logic_id"]}-{data["name"]}')
                 if md:
-                    crwaled_problems.add(data['logic_id'])
+                    self.crawled_problems.add(data['logic_id'])
                 else:
                     self.logger.error(f'Failed to save problem {data["logic_id"]}')
                 pbar.update(1)
             pbar.close()
-        with open((dest / 'crwaled_problems.json').as_posix(), 'w', encoding='utf-8') as f:
-            json.dump(list(crwaled_problems), f)
-        return problems
+        with open((dest / 'crawled.json').as_posix(), 'w', encoding='utf-8') as f:
+            json.dump({
+                'crawled_problems': list(self.crawled_problems),
+                'crawled_answers': list(self.crawled_answers)
+            }, f)
+            
+        # get answers
+        if self.get_answer:
+            resp = self.get(f'{self.base_url}/api/users/{self.user_id}')
+            if not resp.ok:
+                self.logger.error(f'Failed to get answer list from user info: {resp.text}')
+                return
+            self.answers = {acp['logic_id']: self.problems[acp['logic_id']] for acp in resp.json()['ac_problems']}
+            with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
+                tasks = [executor.submit(self.get_answer, prob) for logic_id, prob in self.answers.items() if logic_id not in self.crawled_answers]
+                pbar = tqdm(total=len(tasks), ncols=100, ascii=True)
+                pbar.set_description('Getting answer logic_id: None')
+                for task in concurrent.futures.as_completed(tasks):
+                    data = task.result()
+                    if data is None:
+                        pbar.update(1)
+                        continue
+                    self.answers[data['logic_id']] = data
+                    pbar.set_description(f'Getting answer logic_id: {data["logic_id"]}')
+                    v = self.md_parser.to_v(data, dest / f'{data["logic_id"]}-{data["name"]}')
+                    if v:
+                        self.crawled_answers.add(data['logic_id'])
+                    else:
+                        self.logger.error(f'Failed to save answer {data["logic_id"]}')
+                    pbar.update(1)
+                pbar.close()
+        with open((dest / 'crawled.json').as_posix(), 'w', encoding='utf-8') as f:
+            json.dump({
+                'crawled_problems': list(self.crawled_problems),
+                'crawled_answers': list(self.crawled_answers)
+            }, f)
+            
+        return self.problems, self.answers
 
     
     def get_problem(self, problem):
         resp = self.get(f'{self.base_url}/api/problems/{problem["id"]}/')
         if not resp.ok:
-            self.logger.error(f'Failed to get problem {problem["id"]}: {resp.text}')
+            self.logger.error(f'Failed to get problem {problem["logic_id"]}: {resp.text}')
             return None
         data = resp.json()
         problem.update({
@@ -146,17 +257,58 @@ class VerilogOJCrawler(requests.Session):
         return problem
 
 
-
+    def get_answer(self, problem):
+        resp = self.get(f'{self.base_url}/api/submissions/?user={self.user_id}&problem={problem["id"]}')
+        if not resp.ok:
+            self.logger.error(f'Failed to get answer {problem["logic_id"]}: {resp.text}')
+            return None
+        data = resp.json()
+        if len(data) == 0:
+            self.logger.error(f'No answer for problem {problem["logic_id"]}')
+            return None
+        ac_submission = [sub for sub in data if sub['result'] == 'Accepted']
+        if len(ac_submission) == 0:
+            self.logger.error(f'No accepted answer for problem {problem["logic_id"]}')
+            return None
+        problem.update({'answer_id': ac_submission[0]['id']})
+        resp = self.get(f'{self.base_url}/api/submissions/{problem["answer_id"]}')
+        if not resp.ok:
+            self.logger.error(f'Failed to get answer {problem["logic_id"]}: {resp.text}')
+            return None
+        problem.update({'answer_file_id': resp.json()['submit_file']})
+        resp = self.get(f'{self.base_url}/api/files/{problem["answer_file_id"]}')
+        if not resp.ok:
+            self.logger.error(f'Failed to get answer file {problem["answer_file_id"]}: {resp.text}')
+            return None
+        problem.update({'answer': resp.text.replace('\r\n', '\n')})
+        return problem
 
     
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--url', '-u', type=str, required=True, help='the url of the OJ, should be like http://xxx.xxx.xxx.xxx/oj')
+    parser.add_argument('--url', type=str, required=True, help='the url of the OJ, should be like http://xxx.xxx.xxx.xxx/oj')
     parser.add_argument('--log', '-l', type=str, required=False, help='the log file path', default=None)
     parser.add_argument('--dest', '-d', type=str, required=False, help='the destination directory to save problems', default='problems')
     parser.add_argument('--workers', '-w', type=int, required=False, help='the number of workers to crawl problems', default=4)
-    args = parser.parse_args()
+    parser.add_argument('--get_answer', '-a', action='store_true', help='whether to get the answer of the problems (default: False)', default=False)
+    parser.add_argument('--username', '-u', type=str, required=False, help='the username of the OJ (needed if `get_answer` is True)', default=None)
+    parser.add_argument('--password', '-p', type=str, required=False, help='the password of the OJ (needed if `get_answer` is True)', default=None)
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        exit(0)
     
-    crawler = VerilogOJCrawler(args.url, args.log)
-    problems = crawler.get_all_problems(args.dest, args.workers)
+    # remove the final '/' if exists
+    while args.url[-1] == '/':
+        args.url = args.url[:-1]
+    
+    crawler = VerilogOJCrawler(
+        args.url, 
+        get_answer=args.get_answer, 
+        username=args.username, 
+        password=args.password, 
+        log_file=args.log
+    )
+    problems, answers = crawler.get_all(args.dest, args.workers)
